d0c21d96f3dbccce5c47a6ebf59871e526a7b6c8
niftus13 <niftus13@gmail.com>
2023-07-12 10:48:04 +0900

    first commit

---
+++ src/main/java/org/zerock/jj2/Jj2Application.java
@@ -0,0 +1,13 @@
+package org.zerock.jj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jj2Application.class, args);
+	}
+
+}
